d0d1cc5397044941285bb34470ede23474e6f7fa
Emiliomv14 <159396099+Emiliomv14@users.noreply.github.com>
2024-02-28 20:31:15 -0600

    Create global.js

---
+++ scripts/global.js
@@ -0,0 +1,6 @@
+document.querySelector(".close-window").addEventListener("click", function()
+{
+    console.log("click");
+    document.querySelector(".window").classList.remove("open");
+    //document.querySelector("window").classList.add("open");
+})
