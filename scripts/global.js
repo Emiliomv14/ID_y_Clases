eb06b3da7b6b0a5f67ce75cab6a8d9e0ba44df24
Emiliomv14 <159396099+Emiliomv14@users.noreply.github.com>
2024-03-06 19:12:52 -0600

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -4,3 +4,7 @@ document.querySelector(".close-window").addEventListener("click", function()
     document.querySelector(".window").classList.remove("open");
     //document.querySelector("window").classList.add("open");
 })
+document.querySelector(".open-button").addEventListener("click", function(){
+    console.log("click");
+    document.querySelector(".window").classList.add("open");
+});
